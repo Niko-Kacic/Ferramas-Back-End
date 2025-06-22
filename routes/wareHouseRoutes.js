c8349c8d33952bebaa783341624efd5917dce696
NikoBoolean <niko.kacic@gmail.com>
2025-06-22 19:59:01 -0400

    add stock ti wareHouses added

--- routes/wareHouseRoutes.js
+++ routes/wareHouseRoutes.js
@@ -17,8 +17,8 @@ router.put('/:id', updateWarehouse);
 router.delete('/:id', deleteWarehouse);
 
 
-router.post('/', addStock); 
-router.put('/:warehouse_id/:product_id', updateStock); 
-router.get('/:warehouse_id', getStockByWarehouse); 
+router.post('/stock', addStock); 
+router.put('/stock/:warehouse_id/:product_id', updateStock);
+router.get('/stock/:warehouse_id', getStockByWarehouse);
 
 export default router;
